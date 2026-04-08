de864671e08a02c3c975e9fd57e31f0bcb8c8786
tungvuhoang <tung.vh235241@sis.hust.edu.vn>
2026-04-08 17:59:23 +0700

    setup spring boot project with mysql and base structure

---
+++ quizweb/src/test/java/com/example/quizweb/QuizwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
